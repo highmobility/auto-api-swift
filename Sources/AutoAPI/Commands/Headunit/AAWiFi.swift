4b38bcaa2d6f40a0a558685029c2ad812bdceafc
doofyus <mikk.ratsep@gmail.com>
2018-10-16 16:51:33 +0300

    renamed Wifi.connectToNetwork params

--- Sources/AutoAPI/Commands/Headunit/AAWiFi.swift
+++ Sources/AutoAPI/Commands/Headunit/AAWiFi.swift
@@ -116,10 +116,10 @@ public extension AAWiFi {
     }
 
 
-    static func connectToNetwork(ssid: AANetworkSSID, password: String?, security: AANetworkSecurity) -> [UInt8] {
+    static func connectToNetwork(ssid: AANetworkSSID, security: AANetworkSecurity, password: String?) -> [UInt8] {
         return commandPrefix(for: .connectToNetwork) + [ssid.propertyBytes(0x03),
-                                                        password?.propertyBytes(0x05),
-                                                        security.propertyBytes(0x04)].propertiesValuesCombined
+                                                        security.propertyBytes(0x04),
+                                                        password?.propertyBytes(0x05)].propertiesValuesCombined
     }
 
     static func enableDisalbe(_ state: AAEnabledState) -> [UInt8] {
